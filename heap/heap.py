b490207b14d59257706bdbaa1cf4dde868911d0f
Christopher Harris <cnharris@gmail.com>
2023-11-29 17:58:41 -0500

    Max and min heap

--- heap/heap.py
+++ heap/heap.py
@@ -1,15 +1,17 @@
 import math
 from random import seed, randint
-from typing import List
+from typing import List, Any
 
 
 class Heap:
     values: List[int] = []
 
-    def __init__(self, print_after_operation: bool = True):
+    def __init__(self, max_heap: bool = True, print_after_operation: bool = True):
+        self.max_heap = max_heap
         self.print_after_operation = print_after_operation
+        self.default_value = -math.inf if self.max_heap else math.inf
 
-    def insert(self, value: int) -> None:
+    def insert(self, value: Any) -> None:
         if not self.values:
             self.values = [value]
             return
@@ -19,7 +21,7 @@ class Heap:
         if self.print_after_operation:
             self._print_heap()
 
-    def remove(self, value: int) -> None:
+    def remove(self, value: Any) -> None:
         if value in self.values:
             idx = self.values.index(value)
         else:
@@ -41,8 +43,8 @@ class Heap:
     def size(self):
         return len(self.values)
 
-    def value_at(self, idx: int):
-        return self.values[idx]
+    def value_at(self, index: int):
+        return self.values[index]
 
     def _heapify(self, pos: int) -> None:
         values_length = len(self.values)
@@ -50,16 +52,41 @@ class Heap:
         while parent_pos >= 0:
             left_pos = self._left_pos(parent_pos)
             right_pos = self._right_pos(parent_pos)
-            left = self.values[left_pos] if left_pos < values_length else -math.inf
-            right = self.values[right_pos] if right_pos < values_length else -math.inf
+            left = (
+                self.values[left_pos]
+                if left_pos < values_length
+                else self.default_value
+            )
+            right = (
+                self.values[right_pos]
+                if right_pos < values_length
+                else self.default_value
+            )
 
-            if left > right and left > self.values[parent_pos]:
-                self._swap(parent_pos, left_pos)
-
-            if right > left and right > self.values[parent_pos]:
-                self._swap(parent_pos, right_pos)
+            if self.max_heap:
+                self._max_heap_comparison(left, right, parent_pos, left_pos, right_pos)
+            else:
+                self._min_heap_comparison(left, right, parent_pos, left_pos, right_pos)
 
             parent_pos = self._get_parent_pos(parent_pos)
+
+    def _max_heap_comparison(
+        self, left: Any, right: Any, parent_pos, left_pos, right_pos
+    ):
+        if left > right and left > self.values[parent_pos]:
+            self._swap(parent_pos, left_pos)
+
+        if right > left and right > self.values[parent_pos]:
+            self._swap(parent_pos, right_pos)
+
+    def _min_heap_comparison(
+        self, left: Any, right: Any, parent_pos: int, left_pos: int, right_pos: int
+    ):
+        if left < right and left < self.values[parent_pos]:
+            self._swap(parent_pos, left_pos)
+
+        if right < left and right < self.values[parent_pos]:
+            self._swap(parent_pos, right_pos)
 
     def _swap(self, parent_pos: int, child_pos: int) -> None:
         self.values[parent_pos], self.values[child_pos] = (
@@ -85,7 +112,7 @@ class Heap:
 
 if __name__ == "__main__":
     size = 20
-    heap = Heap()
+    heap = Heap(max_heap=True)
     for i in range(size):
         heap.insert(randint(0, size * 10))
 
