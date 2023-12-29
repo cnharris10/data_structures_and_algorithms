2e198638ca843f15c7527a952d74571310f75437
Christopher Harris <cnharris@gmail.com>
2023-12-28 21:34:47 -0500

    Set

--- search/binary_search.py
+++ search/binary_search.py
@@ -1,13 +1,24 @@
-class BinarySearch(object):
-    def run(self):
-        l = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        self.algorithm(l, 0, 8, 2)
-        self.algorithm(l, 0, 8, 1)
-        self.algorithm(l, 0, 8, 9)
-        self.algorithm(l, 0, 8, 19)
+from typing import Optional
 
-    def algorithm(self, l, start, end, value, last_pos=None):
+
+class BinarySearch(object):
+    @staticmethod
+    def run():
+        l = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        BinarySearch.algorithm(l, 0, 8, 2)
+        BinarySearch.algorithm(l, 0, 8, 1)
+        BinarySearch.algorithm(l, 0, 8, 9)
+        BinarySearch.algorithm(l, 0, 8, 19)
+
+    @staticmethod
+    def algorithm(
+        l: list, start: int, end: int, value: int, last_pos=None
+    ) -> Optional[int]:
         print(f"Searching for: {value}")
+        if not l:
+            print("Result: Not in list")
+            return
+
         pos = round((start + end) / 2)
         current = l[pos]
         if pos == last_pos:
@@ -16,14 +27,14 @@ class BinarySearch(object):
 
         if value == current:
             print(f"Found: {current}")
-            return current
+            return pos
         if current < value:
             print(f"Narrowing to greater window")
-            return self.algorithm(l, pos + 1, end, value, pos)
+            return BinarySearch.algorithm(l, pos + 1, end, value, pos)
         else:
             print(f"Narrowing to lesser window")
-            return self.algorithm(l, start, pos - 1, value, pos)
+            return BinarySearch.algorithm(l, start, pos - 1, value, pos)
 
 
 if __name__ == "__main__":
-    BinarySearch().run()
+    BinarySearch.run()
